fc254ecb0a919e7b034cf568f6185a1669b59164
adibrisan <adi.brisan@yahoo.com>
2023-10-04 10:26:04 +0300

    Logging

--- src/main/java/com/adrian/springbootproject/controller/DepartmentController.java
+++ src/main/java/com/adrian/springbootproject/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.adrian.springbootproject.controller;
 import com.adrian.springbootproject.entity.Department;
 import com.adrian.springbootproject.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +14,16 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController.");
         return departmentService.saveDepartment(department);
     }
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController.");
         return departmentService.fetchDepartmentList();
     }
     @GetMapping("/departments/{id}")
