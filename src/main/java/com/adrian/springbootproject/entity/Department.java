e5f46cd9e626cf6c7b02eec137e35bf3b3855cb0
adibrisan <adi.brisan@yahoo.com>
2023-10-08 19:42:30 +0300

    Change connection to mysql db

--- src/main/java/com/adrian/springbootproject/entity/Department.java
+++ src/main/java/com/adrian/springbootproject/entity/Department.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Department {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long departmentId;
     @NotBlank(message = "Please add department name.")
     private String departmentName;
